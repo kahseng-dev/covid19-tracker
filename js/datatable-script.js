54daed5550dc4417cfbe4d7f21c2710e14cb66ea
kahseng <S10208180@connect.np.edu.sg>
2021-01-08 23:25:31 +0800

    add reverse sort function

--- js/datatable-script.js
+++ js/datatable-script.js
@@ -1,15 +1,82 @@
 const entries = document.getElementById("entries");
-let promiseArray = [];
+let dataArray = [];
 
 $(document).ready(function() {
     loadData();
-    
-    $("#countryButton").on("click", function() { sortData(promiseArray, "byCountry") })
-    $("#countryCodeButton").on("click", function() { sortData(promiseArray, "byCountryCode") })
-    $("#totalCaseButton").on("click", function() { sortData(promiseArray, "byCase") })
-    $("#totalDeathsButton").on("click", function() { sortData(promiseArray, "byDeaths") })
-    $("#totalRecoveredButton").on("click", function() { sortData(promiseArray, "byRecovered") })
-    $("#activeCaseButton").on("click", function() { sortData(promiseArray, "byActive") })
+    var order = false;
+    $(".header").change(function(e) { order = false })
+
+    $("#countryButton").click(function() {
+        if (order == true) {
+            sortData("byCountry", true);
+            order = false;
+        }
+
+        else {
+            sortData("byCountry", false);
+            order = true;
+        }
+    })
+
+    $("#countryCodeButton").click(function() {
+        if (order == true) {
+            sortData("byCountryCode", true);
+            order = false;
+        }
+
+        else {
+            sortData("byCountryCode", false);
+            order = true;
+        }
+    })
+
+    $("#totalCaseButton").click(function() {
+        if (order == true) {
+            sortData("byCase", true);
+            order = false;
+        }
+        
+        else {
+            sortData("byCase", false);
+            order = true;
+        }
+    })
+
+    $("#totalDeathsButton").click(function() {
+        if (order == true) {
+            sortData("byDeaths", true);
+            order = false;
+        }
+        
+        else {
+            sortData("byDeaths", false);
+            order = true;
+        }
+    })
+
+    $("#totalRecoveredButton").click(function() {
+        if (order == true) {
+            sortData("byRecovered", true);
+            order = false;
+        }
+        
+        else {
+            sortData("byRecovered", false);
+            order = true;
+        }
+    })
+
+    $("#activeCaseButton").click(function() {
+        if (order == true) {
+            sortData("byActive", true);
+            order = false;
+        }
+        
+        else {
+            sortData("byActive", false);
+            order = true;
+        }
+    })
 });
 
 const loadData = async() => {
@@ -22,75 +89,62 @@ const loadData = async() => {
         return Promise.all([countryInfo.name, c])
     })
 
-    displayPromiseData(promiseArray);
-}
-
-const displayPromiseData = (promiseArray) => {
-    var i = 0;
-
-    promiseArray.forEach(promise => {
-        promise.then(result => {
-            var htmlString = [
-                `<tr>`,
-                `<td scope="row">${i += 1}</td>`,
-                `<td>${result[0]}</td>`,
-                `<td>${result[1].country}</td>`,
-                `<td>${result[1].cases}</td>`,
-                `<td>${result[1].deaths}</td>`,
-                `<td>${result[1].recovered}</td>`,
-                `<td>${result[1].cases - (result[1].deaths + result[1].recovered)}</td>`,
-                `</tr>`
-            ]
-            entries.innerHTML += htmlString.join('').replace(',','')
-        })
+    Promise.all(promiseArray).then(promisedArray => {
+        displayPromiseData(promisedArray);
     })
 }
 
-
-function sortData(promiseArray, sortOrder) {
+const displayPromiseData = (array) => {
     var i = 0;
+    dataArray = array;
+    dataArray.forEach(data => {
+        var htmlString = [
+            `<tr>`,
+            `<td scope="row">${i += 1}</td>`,
+            `<td>${data[0]}</td>`,
+            `<td>${data[1].country}</td>`,
+            `<td>${data[1].cases}</td>`,
+            `<td>${data[1].deaths}</td>`,
+            `<td>${data[1].recovered}</td>`,
+            `<td>${data[1].cases - (data[1].deaths + data[1].recovered)}</td>`,
+            `</tr>`
+        ]
+        entries.innerHTML += htmlString.join('').replace(',','')
+    })
+}
+
+function sortData(order, mode) {
     entries.innerHTML = "";
 
-    Promise.all(promiseArray).then(dataArray => {
-        dataArray.sort(function(a,b) {
-            switch(sortOrder) {
-                case "byCountry":
-                    if (a[0] < b[0]) { return -1; }
-                    if (a[0] > b[0]) { return 1; }
-                    return 0;
+    dataArray.sort(function(a,b) {
+        switch(order) {
+            case "byCountry":
+                if (a[0] < b[0]) { return -1; }
+                if (a[0] > b[0]) { return 1; }
+                return 0;
 
-                case "byCountryCode":
-                    if (a[1].country < b[1].country) { return -1; }
-                    if (a[1].country > b[1].country) { return 1; }
-                    return 0;
-                    
-                case "byCase":
-                    return b[1].cases - a[1].cases;
-
-                case "byDeaths":
-                    return b[1].deaths - a[1].deaths;
+            case "byCountryCode":
+                if (a[1].country < b[1].country) { return -1; }
+                if (a[1].country > b[1].country) { return 1; }
+                return 0;
                 
-                case "byRecovered":
-                    return b[1].recovered - a[1].recovered;
+            case "byCase":
+                return b[1].cases - a[1].cases;
 
-                case "byActive":
-                    return (b[1].cases - (b[1].deaths + b[1].recovered)) - (a[1].cases - (a[1].deaths + a[1].recovered));
-            }
-        });
-        dataArray.forEach(result => {
-            var htmlString = [
-                `<tr>`,
-                `<td scope="row">${i += 1}</td>`,
-                `<td>${result[0]}</td>`,
-                `<td>${result[1].country}</td>`,
-                `<td>${result[1].cases}</td>`,
-                `<td>${result[1].deaths}</td>`,
-                `<td>${result[1].recovered}</td>`,
-                `<td>${result[1].cases - (result[1].deaths + result[1].recovered)}</td>`,
-                `</tr>`
-            ]
-            entries.innerHTML += htmlString.join('').replace(',','')
-        })
-    })
+            case "byDeaths":
+                return b[1].deaths - a[1].deaths;
+            
+            case "byRecovered":
+                return b[1].recovered - a[1].recovered;
+
+            case "byActive":
+                return (b[1].cases - (b[1].deaths + b[1].recovered)) - (a[1].cases - (a[1].deaths + a[1].recovered));
+        }
+    });
+
+    if (mode == true) {
+        dataArray.reverse();
+    }
+    
+    displayPromiseData(dataArray);
 }
-
