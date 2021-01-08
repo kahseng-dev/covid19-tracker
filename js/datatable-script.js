50d8a96d50e208927f824ed2bb04498a069a4fff
kahseng <S10208180@connect.np.edu.sg>
2021-01-08 23:09:48 +0800

    adjust html and js for datatable page

--- js/datatable-script.js
+++ js/datatable-script.js
@@ -1,44 +1,40 @@
+const entries = document.getElementById("entries");
+let countryStatus = [];
+
 $(document).ready(function() {
-    $(".entries").html(``);
-    createWorldRow();
-    createCountryRow();
+    loadData();
 });
 
-async function createWorldRow() {
-    var url = "https://covid19-api.org/api/timeline";
-    const response = await fetch(url);
-    var data = await response.json();
-    $(".entries").append(
-        `<tr style="background-color: rgba(0,0,0,.075);" >
-            <td scope="row"></td>
-            <td>World</td>
-            <td>${data[0].total_cases}</td>
-            <td>${data[0].total_deaths}</td>
-            <td>${data[0].total_recovered}</td>
-            <td>${data[0].total_cases - (data[0].total_deaths + data[0].total_recovered)}</td>
-        </tr>`
-    );
-}
+const loadData = async() => {
+    const statusRes = await fetch("https://covid19-api.org/api/status");
+    countryStatus = await statusRes.json();
 
-async function createCountryRow() {
-    var countryURL = "https://covid19-api.org/api/countries";
-    const countryResponse = await fetch(countryURL);
-    var countryData = await countryResponse.json();
+    displayCountries(countryStatus);
+};
 
-    for (i = 0; i < countryData.length; i++) {
-        var statusURL = "https://covid19-api.org/api/status/" + countryData[i].alpha2;
-        const statusResponse = await fetch(statusURL);
-        var statusData = await statusResponse.json();
-                
-        $(".entries").append(
-            `<tr>
-                <td scope="row">${i+1}</td>
-                <td>${countryData[i].name}</td>
-                <td>${statusData.cases}</td>
-                <td>${statusData.deaths}</td>
-                <td>${statusData.recovered}</td>
-                <td>${statusData.cases - (statusData.deaths + statusData.recovered)}</td>
-            </tr>`
-        );
-    }
-}
+const displayCountries = (countries) => {
+    var dataArray = [];
+    let i = 0
+
+    countries.map((c) => {
+        var countryArray = [];
+        
+        fetch("https://covid19-api.org/api/country/" + c.country)
+        .then(response => response.json())
+        .then(function(data) {
+            countryArray.push(
+                `<tr>`,
+                `<td scope="row">${i += 1}</td>`,
+                `<td>` + data.name + `</td>`,
+                `<td>${c.country}</td>`,
+                `<td>${c.cases}</td>`,
+                `<td>${c.deaths}</td>`,
+                `<td>${c.recovered}</td>`,
+                `<td>${c.cases - (c.deaths + c.recovered)}</td>`,
+                `</tr>`
+            )
+            dataArray.push(countryArray);
+            entries.innerHTML += countryArray.join('').replace(',','')
+        })
+    })
+};
