a042d7d94a87b889830551e3a9f3cd6b5d492362
kahseng <S10208180@connect.np.edu.sg>
2021-01-08 23:32:25 +0800

    fix loading data issue

--- js/datatable-script.js
+++ js/datatable-script.js
@@ -1,150 +1,106 @@
 const entries = document.getElementById("entries");
-let dataArray = [];
+let countryArray = [];
 
 $(document).ready(function() {
     loadData();
-    var order = false;
-    $(".header").change(function(e) { order = false })
+    let count = 0;
 
-    $("#countryButton").click(function() {
-        if (order == true) {
-            sortData("byCountry", true);
-            order = false;
-        }
+    $(".headerButtons").change(function(e) { count = 0; }) // reset count when user switches to another tab
 
-        else {
-            sortData("byCountry", false);
-            order = true;
-        }
-    })
-
-    $("#countryCodeButton").click(function() {
-        if (order == true) {
-            sortData("byCountryCode", true);
-            order = false;
-        }
-
-        else {
-            sortData("byCountryCode", false);
-            order = true;
-        }
-    })
-
-    $("#totalCaseButton").click(function() {
-        if (order == true) {
-            sortData("byCase", true);
-            order = false;
-        }
-        
-        else {
-            sortData("byCase", false);
-            order = true;
-        }
-    })
-
-    $("#totalDeathsButton").click(function() {
-        if (order == true) {
-            sortData("byDeaths", true);
-            order = false;
-        }
-        
-        else {
-            sortData("byDeaths", false);
-            order = true;
-        }
-    })
-
-    $("#totalRecoveredButton").click(function() {
-        if (order == true) {
-            sortData("byRecovered", true);
-            order = false;
-        }
-        
-        else {
-            sortData("byRecovered", false);
-            order = true;
-        }
-    })
-
-    $("#activeCaseButton").click(function() {
-        if (order == true) {
-            sortData("byActive", true);
-            order = false;
-        }
-        
-        else {
-            sortData("byActive", false);
-            order = true;
-        }
-    })
+    $("#countryButton").click(function() { count = checkSort("byCountry", count) })
+    $("#countryCodeButton").click(function() { count = checkSort("byCountryCode", count) })
+    $("#totalCaseButton").click(function() { count = checkSort("byCase", count) })
+    $("#totalDeathsButton").click(function() { count = checkSort("byDeaths", count) })
+    $("#totalRecoveredButton").click(function() { count = checkSort("byRecovered", count) })
+    $("#activeCaseButton").click(function() { count = checkSort("byActive", count); })
 });
 
-const loadData = async() => {
+function checkSort(order, count) {
+    if (count == 0) {
+        sortData(order, false);
+        return count = 1;
+    } else {
+        sortData(order, true);
+        return count = 0;
+    }
+}
+
+async function loadData() {
     const statusRes = await fetch("https://covid19-api.org/api/status");
     const countryStatus = await statusRes.json();
-
-    promiseArray = countryStatus.map(async (c) => {
-        const countryRes = await fetch("https://covid19-api.org/api/country/" + c.country);
-        const countryInfo = await countryRes.json();
-        return Promise.all([countryInfo.name, c])
-    })
-
-    Promise.all(promiseArray).then(promisedArray => {
-        displayPromiseData(promisedArray);
-    })
-}
-
-const displayPromiseData = (array) => {
-    var i = 0;
-    dataArray = array;
-    dataArray.forEach(data => {
-        var htmlString = [
-            `<tr>`,
-            `<td scope="row">${i += 1}</td>`,
-            `<td>${data[0]}</td>`,
-            `<td>${data[1].country}</td>`,
-            `<td>${data[1].cases}</td>`,
-            `<td>${data[1].deaths}</td>`,
-            `<td>${data[1].recovered}</td>`,
-            `<td>${data[1].cases - (data[1].deaths + data[1].recovered)}</td>`,
-            `</tr>`
-        ]
-        entries.innerHTML += htmlString.join('').replace(',','')
-    })
-}
-
-function sortData(order, mode) {
-    entries.innerHTML = "";
-
-    dataArray.sort(function(a,b) {
-        switch(order) {
-            case "byCountry":
-                if (a[0] < b[0]) { return -1; }
-                if (a[0] > b[0]) { return 1; }
-                return 0;
-
-            case "byCountryCode":
-                if (a[1].country < b[1].country) { return -1; }
-                if (a[1].country > b[1].country) { return 1; }
-                return 0;
-                
-            case "byCase":
-                return b[1].cases - a[1].cases;
-
-            case "byDeaths":
-                return b[1].deaths - a[1].deaths;
-            
-            case "byRecovered":
-                return b[1].recovered - a[1].recovered;
-
-            case "byActive":
-                return (b[1].cases - (b[1].deaths + b[1].recovered)) - (a[1].cases - (a[1].deaths + a[1].recovered));
-        }
-    });
-
-    if (mode == true) {
-        dataArray.reverse();
-    }
     
-    displayPromiseData(dataArray);
+    const countriesRes = await fetch("https://covid19-api.org/api/countries");
+    const countriesData = await countriesRes.json();
+
+    countryStatus.map((s) => {
+        countriesData.map((c) => { 
+            if (c.alpha2 == s.country) {
+                var country = Object.assign(s, c, {"active": s.cases - (s.deaths + s.recovered)});
+                delete country["alpha2"];
+                countryArray.push(country);
+            }
+        })
+    })
+
+    displayData(countryArray);
+}
+
+function displayData(array) {
+    const htmlString = array.map((data) => {
+        return `
+        <tr>
+            <td scope="row">${array.indexOf(data) + 1}</td>
+            <td>${data.name}</td>
+            <td>${data.country}</td>
+            <td>${data.cases}</td>
+            <td>${data.deaths}</td>
+            <td>${data.recovered}</td>
+            <td>${data.active}</td>
+        </tr>`
+    })
+    entries.innerHTML += htmlString.join('').replace(',','')
+}
+
+function sortData(order, reverse) {
+    entries.innerHTML = "";
+    
+    switch(order) {
+        case "byCountry":
+            countryArray.sort(function(a, b) {
+                if (a.name < b.name) { return -1; }
+                if (a.name > b.name) { return 1; }
+                return 0;
+            });
+            break;
+
+        case "byCountryCode":
+            countryArray.sort(function(a, b) {
+                if (a.country < b.country) { return -1; }
+                if (a.country > b.country) { return 1; }
+                return 0;
+            });
+            break;
+            
+        case "byCase": 
+            countryArray.sort(function(a, b) { return b.cases - a.cases });
+            break;
+
+        case "byDeaths": 
+            countryArray.sort(function(a, b) { return b.deaths - a.deaths })
+            break;
+
+        case "byRecovered": 
+            countryArray.sort(function(a, b) { return b.recovered - a.recovered })
+            break;
+
+        case "byActive": 
+            countryArray.sort(function(a, b) { return b.active - a.active })
+            break;
+    }
+
+    if (reverse == true) { 
+        countryArray.reverse(); 
+    } 
+
+    displayData(countryArray);
 }
