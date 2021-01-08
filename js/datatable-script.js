6fd05cffd178c6a1b2efbcd8fa7fec8bd3b569ae
kahseng <S10208180@connect.np.edu.sg>
2021-01-08 23:18:54 +0800

    change loadData function to load faster

--- js/datatable-script.js
+++ js/datatable-script.js
@@ -1,40 +1,38 @@
 const entries = document.getElementById("entries");
-let countryStatus = [];
-
-$(document).ready(function() {
-    loadData();
-});
 
 const loadData = async() => {
     const statusRes = await fetch("https://covid19-api.org/api/status");
-    countryStatus = await statusRes.json();
+    const countryStatus = await statusRes.json();
 
-    displayCountries(countryStatus);
-};
+    const dataArray = countryStatus.map(async (c) => {
+        const countryRes = await fetch("https://covid19-api.org/api/country/" + c.country);
+        const countryInfo = await countryRes.json();
 
-const displayCountries = (countries) => {
-    var dataArray = [];
-    let i = 0
+        return Promise.all([countryInfo.name, c])
+    })
+    displayData(dataArray)
+}
 
-    countries.map((c) => {
-        var countryArray = [];
-        
-        fetch("https://covid19-api.org/api/country/" + c.country)
-        .then(response => response.json())
-        .then(function(data) {
-            countryArray.push(
+const displayData = (dataArray) => {
+    var i = 0;
+    
+    dataArray.forEach(data => {
+        data.then(result => {
+            console.log(result)
+            var htmlString = [
                 `<tr>`,
                 `<td scope="row">${i += 1}</td>`,
-                `<td>` + data.name + `</td>`,
-                `<td>${c.country}</td>`,
-                `<td>${c.cases}</td>`,
-                `<td>${c.deaths}</td>`,
-                `<td>${c.recovered}</td>`,
-                `<td>${c.cases - (c.deaths + c.recovered)}</td>`,
+                `<td>${result[0]}</td>`,
+                `<td>${result[1].country}</td>`,
+                `<td>${result[1].cases}</td>`,
+                `<td>${result[1].deaths}</td>`,
+                `<td>${result[1].recovered}</td>`,
+                `<td>${result[1].cases - (result[1].deaths + result[1].recovered)}</td>`,
                 `</tr>`
-            )
-            dataArray.push(countryArray);
-            entries.innerHTML += countryArray.join('').replace(',','')
+            ]
+            entries.innerHTML += htmlString.join('').replace(',','')
         })
     })
-};
+}
+
+loadData()
