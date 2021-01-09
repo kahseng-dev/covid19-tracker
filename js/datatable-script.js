dd52953488d1562a8a187f52000d917c3ad6ad3e
kahseng <S10208180@connect.np.edu.sg>
2021-01-09 16:16:12 +0800

    add total and new tabs to swap between data types

--- js/datatable-script.js
+++ js/datatable-script.js
@@ -5,14 +5,26 @@ $(document).ready(function() {
     loadData();
     let count = 0;
 
-    $(".headerButtons").change(function(e) { count = 0; }) // reset count when user switches to another table tab
+    $("#totalBtn").click(function(e) {
+        $(".casesTab").html("Total Cases")
+        $(".deathsTab").html("Total Deaths")
+        $(".recoveredTab").html("Total Recovered")
+        loadData();
+        $(".active").show();
+    });
 
-    $("#countryButton").click(function() { count = checkSort("byCountry", count) })
-    $("#countryCodeButton").click(function() { count = checkSort("byCountryCode", count) })
-    $("#totalCaseButton").click(function() { count = checkSort("byCase", count) })
-    $("#totalDeathsButton").click(function() { count = checkSort("byDeaths", count) })
-    $("#totalRecoveredButton").click(function() { count = checkSort("byRecovered", count) })
-    $("#activeCaseButton").click(function() { count = checkSort("byActive", count); })
+    $("#newBtn").click(function(e) {
+        loadNewData();
+    });
+
+    $(".headerButtons").change(function(e) { count = 0; }); // reset count when user switches to another table tab
+
+    $("#countryButton").click(function() { count = checkSort("byCountry", count) });
+    $("#countryCodeButton").click(function() { count = checkSort("byCountryCode", count) });
+    $("#totalCaseButton").click(function() { count = checkSort("byCase", count) });
+    $("#totalDeathsButton").click(function() { count = checkSort("byDeaths", count) });
+    $("#totalRecoveredButton").click(function() { count = checkSort("byRecovered", count) });
+    $("#activeCaseButton").click(function() { count = checkSort("byActive", count); });
 
     searchBar.addEventListener('keyup', (e) => {
         const searchString = e.target.value.toLowerCase();
@@ -61,7 +73,8 @@ async function loadData() {
     countryStatus.map((s) => {
         countriesData.map((c) => { 
             if (c.alpha2 == s.country) {
-                var country = Object.assign(s, c, {active: s.cases - (s.deaths + s.recovered)});
+                var country = Object.assign(
+                    s, c, {active: s.cases - (s.deaths + s.recovered)});
                 delete country["alpha2"];
                 countryArray.push(country);
             }
@@ -82,10 +95,10 @@ function displayData(array) {
         <td class="index"></td>
         <td class="countryName">${array[0].name}</td>
         <td class="countryCode">World</td>
-        <td class="totalCase">${array[0].cases}</td>
-        <td class="totalDeaths">${array[0].deaths}</td>
-        <td class="totalRecovered">${array[0].recovered}</td>
-        <td class="activeCases">${array[0].active}</td>
+        <td class="cases">${array[0].cases}</td>
+        <td class="deaths">${array[0].deaths}</td>
+        <td class="recovered">${array[0].recovered}</td>
+        <td class="active">${array[0].active}</td>
     </tr>`
     )
     entries.innerHTML = htmlString
@@ -96,13 +109,75 @@ function displayData(array) {
             <td class="index">${array[1].indexOf(data) + 1}</td>
             <td class="countryName">${data.name}</td>
             <td class="countryCode">${data.country}</td>
-            <td class="totalCase">${data.cases}</td>
-            <td class="totalDeaths">${data.deaths}</td>
-            <td class="totalRecovered">${data.recovered}</td>
-            <td class="activeCases">${data.active}</td>
+            <td class="cases">${data.cases}</td>
+            <td class="deaths">${data.deaths}</td>
+            <td class="recovered">${data.recovered}</td>
+            <td class="active">${data.active}</td>
         </tr>`
     })
     entries.innerHTML += htmlString.join('')
+}
+
+async function loadNewData() {
+    $("#entries").html("")
+    $(".casesTab").html("New Cases")
+    $(".deathsTab").html("New Deaths")
+    $(".recoveredTab").html("New Recovered")
+    $(".active").hide();
+
+    const worldRes = await fetch("https://covid19-api.org/api/timeline");
+    const worldData = await worldRes.json();
+    
+    const diffRes = await fetch("https://covid19-api.org/api/diff");
+    const countryDiff = await diffRes.json();
+    
+    const countriesRes = await fetch("https://covid19-api.org/api/countries");
+    const countriesData = await countriesRes.json();
+
+    var world = {
+        name: "World",
+        last_update: worldData[0].last_update,
+        cases: worldData[0].total_cases - worldData[1].total_cases,
+        deaths: worldData[0].total_deaths - worldData[1].total_deaths,
+        recovered: worldData[0].total_recovered - worldData[1].total_recovered
+    }
+
+    $("#entries").html(`
+    <tr style="background-color:#F5F5F5; border-top: 1px solid #444;">
+        <td class="index"></td>
+        <td class="countryName">${world.name}</td>
+        <td class="countryCode">World</td>
+        <td class="cases">${world.cases}</td>
+        <td class="deaths">${world.deaths}</td>
+        <td class="recovered">${world.recovered}</td>
+    </tr>
+    `)
+
+    var countryArray = []
+    countriesData.map((c) => {
+        countryDiff.map((d) => { 
+            if (c.alpha2 == d.country) {
+                var country = Object.assign(c,{country:d.country}, {cases: d.new_cases}, {deaths: d.new_deaths}, {recovered: d.new_recovered});
+                delete country["alpha2"];
+                countryArray.push(country);
+            }
+        })
+    })
+
+    countryArray.map((c) => {
+        $("#entries").append(`
+        <tr>
+            <td class="index">${countryArray.indexOf(c) + 1}</td>
+            <td class="countryName">${c.name}</td>
+            <td class="countryCode">${c.country}</td>
+            <td class="cases">${c.cases}</td>
+            <td class="deaths">${c.deaths}</td>
+            <td class="recovered">${c.recovered}</td>
+        </tr>
+        `)
+    })
+    
+    dataArray = [world, countryArray];
 }
 
 function sortData(order, reverse) {
